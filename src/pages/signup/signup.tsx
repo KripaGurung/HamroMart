0f16c2bba845d5e017238de5a8dadaba1009389c
KripaGurung <krepagurung@gmail.com>
2025-12-24 10:52:44 +0545

    Install all the necessary libraries

---
+++ src/pages/signup/signup.tsx
@@ -0,0 +1,16 @@
+const Signup  = () => {
+    return (
+        <div className= "signupContainer">
+
+            <div className="signupBox">
+                <h2>Registeration Form</h2>
+
+                <div className="formContainer">
+                    <form></form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Signup;
