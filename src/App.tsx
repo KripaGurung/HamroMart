14a77d9c0ee45c08cd9d2ae17eb7f4401185a122
KripaGurung <krepagurung@gmail.com>
2025-12-25 16:57:22 +0545

    navbar was addded in component folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Navbar from './componennt/navbar/navbar';
 import Signup from './pages/signup/signup';
 import Login from './pages/login/login';
+import Home from './pages/home/home';
 
 function AppContent() {
+  const location = useLocation();
+
+  const navbarPaths = ['/home'];
 
   return (
     <>
-
-       <ToastContainer position="top-center" autoClose={2000} />
+      {navbarPaths.includes(location.pathname) && <Navbar />}
+      
+      <ToastContainer position="top-center" autoClose={2000} />
 
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   )
---
+++ src/componennt/navbar/navbar.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import "./navbar.css";
+
+const Navbar = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="navbarContainer">
+            <div className="navbarBox">
+                <ul className="navLinks">
+                    <li onClick={() => navigate("/home")}>Home</li>
+                    <li onClick={() => navigate("/products")}>Products</li>
+                    <li onClick={() => navigate("/cart")}>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
