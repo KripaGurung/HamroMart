0eec2dc2edf90ed294f38165c198a851681dfadd
KripaGurung <krepagurung@gmail.com>
2026-02-09 12:31:40 +0545

    add console logs for auth state debugging

--- src/context/auth/AuthProvider.tsx
+++ src/context/auth/AuthProvider.tsx
@@ -11,14 +11,18 @@ interface User {
 
 const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
+  console.log("AuthProvider current user state:", user);
 
   const login = (userData: User) => {
+    console.log("Login function called with:", userData);
     setUser(userData);
-    console.log("User logged in from AuthProvider: ",userData);
+    console.log("User set in AuthProvider:", userData);
   };
 
   const logout = () => {
+    console.log("Logout function called");
     setUser(null);
+    console.log("User cleared");
   };
 
   return (
--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -75,23 +75,13 @@ const Profile: React.FC = () => {
       <div className="profileCard">
         <div className="profileHeader"></div>
 
-        <img
-          src={user.image}
-          alt="profile"
-          className="profileImg"
-        />
+        <img src={user.image} alt="profile" className="profileImg"/>
 
-        <h2>
-          {user.firstName} {user.lastName}
-        </h2>
+        <h2> {user.firstName} {user.lastName} </h2>
 
-        <p className="job">
-          {user.company.title} at {user.company.name}
-        </p>
+        <p className="job"> {user.company.title} at {user.company.name} </p>
 
-        <span className="memberId">
-          Member ID #{user.id}
-        </span>
+        <span className="memberId"> Member ID #{user.id} </span>
 
         <div className="infoSection">
           <div className="infoBox">
@@ -103,10 +93,8 @@ const Profile: React.FC = () => {
           <div className="infoBox">
             <h4>Address</h4>
             <p>{user.address.address}</p>
-            <p>
-              {user.address.city}, {user.address.state}
-            </p>
-            <p>{user.address.postalCode}</p>
+            <p> {user.address.city}, {user.address.state} </p>
+            <p> {user.address.postalCode} </p>
           </div>
         </div>
       </div>
